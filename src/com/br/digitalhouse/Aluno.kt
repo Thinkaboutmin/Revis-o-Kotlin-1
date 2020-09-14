c785387ee94ec86a17c10d90a0401d087f315a95
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-14 16:56:33 -0300

    Implementação do Aluno

--- src/com/br/digitalhouse/Aluno.kt
+++ src/com/br/digitalhouse/Aluno.kt
@@ -7,6 +7,23 @@ package com.br.digitalhouse
  */
 
 
-class Aluno {
+class Aluno(val ra: Int, val nome: String, val sobrenome: String) {
+    init {
+        if (nome.isEmpty()) {
+            println("Nome está vazio.")
+        }
+        if (sobrenome.isEmpty()) {
+            println("Sobrenome está vazio.")
+        }
+    }
 
+    fun assistirAula(aula: Aula) {
+        println("O aluno $nome $sobrenome está assistindo a aula")
+        // TODO: Sem materia ainda...
+    }
+
+    fun fazerLicao(aula: Aula) {
+        // TODO: Classe para lição
+        println("O aluno $nome $sobrenome está realizando a a lição de casa.")
+    }
 }
